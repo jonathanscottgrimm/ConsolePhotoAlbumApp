4b541a71be25358737ee5308114ad688fb7b4d79
Jonathan Grimm <thepantheonofthechosen@gmail.com>
2024-02-16 15:07:12 -0500

    Add project files.

---
+++ ConsolePhotoAlbumApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+
+namespace ConsolePhotoAlbumApp
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var photos = await GetAllPhotosAsync();
+            GroupAndDisplayPhotos(photos);
+        }
+
+        private static async Task<List<Photo>> GetAllPhotosAsync()
+        {
+            using var httpClient = new HttpClient();
+
+            string apiUrl = "https://jsonplaceholder.typicode.com/photos";
+            var response = await httpClient.GetStringAsync(apiUrl);
+            var photos = JsonConvert.DeserializeObject<List<Photo>>(response);
+
+            return photos ?? new List<Photo>();
+        }
+
+        private static void GroupAndDisplayPhotos(List<Photo> photos)
+        {
+            var groupedPhotos = new Dictionary<int, List<Photo>>();
+
+            foreach (var photo in photos)
+            {
+                if (!groupedPhotos.ContainsKey(photo.AlbumId))
+                    groupedPhotos[photo.AlbumId] = new List<Photo>();
+
+                groupedPhotos[photo.AlbumId].Add(photo);
+            }
+
+            foreach (var album in groupedPhotos)
+            {
+                foreach (var photo in album.Value)
+                {
+                    Console.WriteLine($"photo-album {album.Key} [{photo.Id}] {photo.Title}");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
